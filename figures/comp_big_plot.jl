73b7896ec7df2c522538030fd7f58ba6335dba95
christianhilscher <christian.hilscher@protonmail.com>
2021-08-19 18:54:18 +0200

    redo structure for plots

--- figures/comp_big_plot.jl
+++ figures/comp_big_plot.jl
@@ -2,11 +2,12 @@ using JLD2
 using Statistics
 using DataFrames
 using Gadfly
+using CSV
 
 import Cairo, Fontconfig
 
 ### Load data
-load_dict = load("data/comp_big.jld2")
+load_dict = load("data/comp_friedman.jld2")
 res = load_dict["res"]
 
 gdf = groupby(res, [:n, :Approach])
@@ -22,13 +23,44 @@ plot_df = combine(gdf, :bias => mean,
 
 
 p = plot(Scale.color_discrete_manual("#011627", "#ffc000", "#E71D36", "#0F7173"),Guide.xticks(ticks=[0, 5000, 10000, 15000]))
-push!(p, layer(plot_df, x=:n, y=:mse_mean, ymin=:mse_min, ymax=:mse_max, color=:Approach, Geom.line, Geom.ribbon, alpha=[0.6]))
+push!(p, layer(plot_df, x=:n, y=:variance_mean, ymin=:variance_min, ymax=:variance_max, color=:Approach, Geom.line, Geom.ribbon, alpha=[0.6]))
 
 push!(p, Guide.YLabel(""))
 push!(p, Guide.XLabel("Observations n"))
-push!(p, Guide.title("MSE"))
+push!(p, Guide.title("Variance"))
 
-# push!(p, Theme(key_position=:none))
+push!(p, Theme(key_position=:none))
 
 ### Output figure
-draw(PNG("figures/graphs/comp_big_mse.png", 20cm, 12cm, dpi=300), p)
+draw(PNG("figures/graphs/comp/friedman_variance.png", 20cm, 12cm, dpi=300), p)
+
+### Make table
+
+function comp_table(df::DataFrame)
+    
+    # Selecting those values with CART Approach as baseline
+    base_df = df[df.Approach .== "CART", :]
+
+    rename!(base_df, :bias_mean => :bias_base,
+                        :variance_mean => :variance_base,
+                        :mse_mean => :mse_base)
+
+    # Merging onto original frame
+    out_df = leftjoin(df, base_df[!, [:n, :bias_base, :variance_base, :mse_base]], on=:n)
+
+    # Getting percentages
+    out_df[!, :bias_diff] = round.(out_df[!, :bias_mean] ./ out_df[!, :bias_base] .- 1, digits = 3)
+    out_df[!, :variance_diff] = round.(out_df[!, :variance_mean] ./ out_df[!, :variance_base] .- 1, digits=3)
+    out_df[!, :mse_diff] = round.(out_df[!, :mse_mean] ./ out_df[!, :mse_base] .- 1, digits = 3)
+    
+    return out_df[!, [:n, :Approach, :bias_diff, :variance_diff, :mse_diff]]
+end
+
+
+
+tmp = comp_table(plot_df)
+
+variable = :mse_diff
+out_table = unstack(tmp[!, [:n, :Approach, variable]], :Approach, variable)
+
+CSV.write("figures/graphs/comp/friedman_mse.csv", out_table)
